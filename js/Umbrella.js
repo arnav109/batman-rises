754439261ab3598b2f762c9bfc5d8b3fe638e74f
arnav109 <73059228+arnav109@users.noreply.github.com>
2021-05-18 12:24:59 +0530

    Add files via upload

---
+++ js/Umbrella.js
@@ -0,0 +1,20 @@
+class Umbrella {
+    constructor(x,y){
+        var options = {
+            'isStatic': true,
+        }
+        this.body = Bodies.circle(x,y,50,options);
+        this.radius = 50;
+        this.image=loadImage("images/walking_1.png,images/walking_2.png,images/walking_3.png,images/walking_4.png,images/walking_5.png,images/walking_6.png,images/walking_7.png,images/walking_8.png");
+        World.add(world, this.body)
+        //load Image for BestMan
+        
+    }
+
+    display(){
+        var pos = this.body.position;
+        imageMode(CENTER);
+        //display the image for BestMan if the frameCount is over 200, otherwise display the boy with umbrella image
+        image(this.image,pos.x,pos.y+70,300,300);
+    }
+}
